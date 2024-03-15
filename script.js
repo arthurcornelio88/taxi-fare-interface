403b93576416432e8c018e9ee3cebe6b824d7664
arthurcornelio <arthur.cornelio@gmail.com>
2024-03-15 12:43:15 +0100

    Done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
-
+// sadasasd
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
